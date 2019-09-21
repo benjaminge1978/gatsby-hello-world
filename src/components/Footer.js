f9c67815a7b7e802ffee9a87d353aa855bf8ffbb
Ben Davidson <bendavidson@Bens-MacBook-Pro.local>
2019-09-21 21:04:15 +0200

    Footer Image Blocks Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,31 +2,58 @@ import React from 'react'
 import globalStyles from '../global-styles.module.css'
 import footerStyles from './styles/footer.module.css'
 import norskIcon from '../img/ny-norge-map.svg'
+import footerImage from '../img/pexels-photo-325876.jpg'
+import footerImageNext from '../img/arkiv.jpg'
 
 const Footer = () => {
     return (
-        <div className= {footerStyles.blueBg}>
-          <div className = {globalStyles.container}>
-              <div className={footerStyles.iconBlock}>
-                  <div>
-                  <img className = {footerStyles.blockIcons} src = {norskIcon} alt= "Norweigan map logo"></img>
-                      <h5>Landsdekkende</h5>
-                  </div>
-                  <div>
-                  <img className = {footerStyles.blockIcons} src = {norskIcon} alt= "Norweigan map logo"></img>
-                      <h5>Landsdekkende</h5>
-                  </div>
-                  <div>
-                  <img className = {footerStyles.blockIcons} src = {norskIcon} alt= "Norweigan map logo"></img>
-                      <h5>Landsdekkende</h5>
-                  </div>
-                  <div>
-                  <img className = {footerStyles.blockIcons} src = {norskIcon} alt= "Norweigan map logo"></img>
-                      <h5>Landsdekkende</h5>
-                  </div>
-              </div>
-          </div>
-        </div>
+        <footer>
+            <div className={footerStyles.blueBg}>
+                <div className={globalStyles.container}>
+                    <div className={footerStyles.iconBlock}>
+                        <div>
+                            <img className={footerStyles.blockIcons} src={norskIcon} alt="Norweigan map logo"></img>
+                            <h5>Landsdekkende</h5>
+                        </div>
+                        <div>
+                            <img className={footerStyles.blockIcons} src={norskIcon} alt="Norweigan map logo"></img>
+                            <h5>Landsdekkende</h5>
+                        </div>
+                        <div>
+                            <img className={footerStyles.blockIcons} src={norskIcon} alt="Norweigan map logo"></img>
+                            <h5>Landsdekkende</h5>
+                        </div>
+                        <div>
+                            <img className={footerStyles.blockIcons} src={norskIcon} alt="Norweigan map logo"></img>
+                            <h5>Landsdekkende</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={footerStyles.greyBg}>
+                <div className={globalStyles.container}>
+                    <div className={footerStyles.footerBlockWrapper}>
+                        <div className={footerStyles.footerBlock}>
+                            <a href="">Hello</a>
+                            <img src={footerImage} alt="Norweigan map logo"></img>
+                            <p>Vi har mange dyktige advokater innen immaterielle rettigheter, it og media. Se oversikten over alle våre advokater her.</p>
+                        </div>
+                        <div className={footerStyles.footerBlock}>
+                            <a href="">Hello</a>
+                            <img src={footerImageNext} alt="Norweigan map logo"></img>
+                            <p>Vi har mange dyktige advokater innen immaterielle rettigheter, it og media. Se oversikten over alle våre advokater her.</p>
+                        </div>
+                        <div className={footerStyles.footerBlock}>
+                            <a href="">Hello</a>
+                            <img src={footerImage} alt="Norweigan map logo"></img>
+                            <p>Vi har mange dyktige advokater innen immaterielle rettigheter, it og media. Se oversikten over alle våre advokater her.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </footer >
     )
 }
 
