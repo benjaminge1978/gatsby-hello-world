4800cb2c4e5c8eccaa8080b3b3334b9165551dbf
Ben Davidson <bendavidson@Bens-MacBook-Pro.local>
2019-09-21 20:08:12 +0200

    Footer added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import Header from '../components/Header'
 import Article from '../components/Article'
+import Footer from '../components/Footer'
 import '../normalise.css'
 import '../fontstyles.css'
 import globalStyles from '../global-styles.module.css'
 
 const Layout = (props) => {
     return (
-    <div>
-        <Header />
-        <div className = {globalStyles.container}>
-        <Article>
-        {props.children}
-        </Article>
+        <div>
+            <Header />
+            <div className={globalStyles.container}>
+                <div className={globalStyles.article}>
+                    <Article>
+                        {props.children}
+                    </Article>
+                </div>
+            </div>
+            <Footer />
+
         </div>
-       
-    </div>
     )
 }
 
